2da11bceb72553b7068ee984adf010984072d553
rongirl <gro.pretty.arabella@mail.ru>
2023-02-20 14:30:29 +0300

    Inital commit

---
+++ homework1/Factorial/Factorial/Program.fs
@@ -0,0 +1,9 @@
+﻿printfn("Enter number: ")
+let input = System.Console.ReadLine()
+let number = input |> int
+let rec factorial number acc =
+    if number = 0 then 
+        acc
+    else 
+        factorial(number - 1) (acc * number)
+printfn $"Result: {factorial number 1}"
