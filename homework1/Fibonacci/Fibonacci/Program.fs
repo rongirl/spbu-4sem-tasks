d85367ea96da8026659fb06ecb7e8c50b447d821
rongirl <gro.pretty.arabella@mail.ru>
2023-02-20 17:53:35 +0300

    Inital commit

---
+++ homework1/Fibonacci/Fibonacci/Program.fs
@@ -0,0 +1,16 @@
+﻿System.Console.WriteLine("Enter number")
+let input = System.Console.ReadLine()
+let number = input |> int
+let fibonacci number =
+    if number = 0 then
+        0
+    elif  number = 1 then
+        1
+    else    
+        let rec subFibonacci i previous current = 
+            if i = number then
+                previous
+            else    
+                subFibonacci (i + 1) current (previous + current)
+        subFibonacci 1 0 1
+printfn $"%d{fibonacci number}"
