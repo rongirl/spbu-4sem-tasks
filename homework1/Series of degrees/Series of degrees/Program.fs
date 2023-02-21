b93b0d517b260557a0f57bef03e4327bbe9afea5
rongirl <gro.pretty.arabella@mail.ru>
2023-02-21 15:14:51 +0300

    Initial commit

---
+++ homework1/Series of degrees/Series of degrees/Program.fs
@@ -0,0 +1,13 @@
+﻿printfn "%s" "Enter n and m: "
+let input = System.Console.ReadLine().Split(' ')
+let n = input[0] |> int
+let m = input[1] |> int 
+let seriesOfDegrees n m = 
+    let rec loop current list =
+        if current = m then
+            list
+        else 
+            loop (current + 1) ((list |> List.head |> (*) 2) :: list)
+    loop 0 [pown 2 n]
+let listOfDegrees =  seriesOfDegrees n m  |> List.rev
+printfn $"%A{listOfDegrees}"
