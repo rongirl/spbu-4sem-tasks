fa81a300e22adab474bd731569732c9f726da6e0
rongirl <gro.pretty.arabella@mail.ru>
2023-02-24 15:12:44 +0300

    Initial commit

---
+++ homework1/Finding a number/Finding a number/Program.fs
@@ -0,0 +1,21 @@
+﻿let searchNumber number list = 
+    let rec search list index =
+        match list with 
+        | [] -> None
+        | head :: tail when head = number -> Some(index)
+        | head :: tail -> search (tail) (index + 1)
+    search list 0
+
+printfn "Enter length of list: "
+let length = System.Console.ReadLine() |> int
+printfn "Enter elements of list: "
+let input = System.Console.ReadLine().Split(' ')
+let list = List.init length (fun i -> input[i] |> int)
+printfn "Enter number "
+let number = System.Console.ReadLine() |> int
+
+let position = searchNumber number list 
+match position with 
+    | None -> printfn "The element is not in the list"
+    | Some(result) -> printfn "Position of %d in list %d" number result
+    
