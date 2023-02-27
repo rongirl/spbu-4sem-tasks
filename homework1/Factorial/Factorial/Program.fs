c6b9e4162fadedd8e2d9594c78a381a6dc9221fb
rongirl <gro.pretty.arabella@mail.ru>
2023-02-27 17:41:20 +0300

    Add condition number < 0

--- homework1/Factorial/Factorial/Program.fs
+++ homework1/Factorial/Factorial/Program.fs
@@ -1,9 +1,13 @@
 ﻿printfn("Enter number: ")
 let input = System.Console.ReadLine()
 let number = input |> int
-let rec factorial number acc =
-    if number = 0 then 
-        acc
-    else 
-        factorial (number - 1) (acc * number)
-printfn $"Result: {factorial number 1}"
+let factorial number = 
+    let rec subFactorial number acc =
+        if number < 0 then
+            invalidArg (nameof number) "Number is negative"
+        elif number = 0 then 
+            acc
+        else 
+            subFactorial (number - 1) (acc * number)
+    subFactorial number 1
+printfn $"Result: {factorial number}"
