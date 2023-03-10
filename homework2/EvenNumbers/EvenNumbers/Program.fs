843294086a0cdb92a910be55108ff063d020b327
rongirl <gro.pretty.arabella@mail.ru>
2023-03-10 15:55:21 +0300

    Added task

---
+++ homework2/EvenNumbers/EvenNumbers/Program.fs
@@ -0,0 +1,14 @@
+﻿module EvenNumbers
+
+let numberOfEvenNumbersFold list =
+    list |> List.fold(fun acc number -> 
+        if number % 2 = 0 then 
+            acc + 1 else acc) 0
+
+let numberOfEvenNumbersMap list =
+    list |> List.map(fun number -> if number % 2 = 0 then 1 else 0) |> List.sum
+
+let numberOfEvenNumbersFilter list =
+    list |> List.filter(fun number -> number % 2 = 0) |> List.length
+
+let r = numberOfEvenNumbersMap [-4 .. 5]
