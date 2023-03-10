eb91706a367df06eebfd8dff02ced1b99e672116
rongirl <gro.pretty.arabella@mail.ru>
2023-03-10 18:57:15 +0300

    Added task

---
+++ homework2/MapForTrees/MapForTrees/Program.fs
@@ -0,0 +1,12 @@
+﻿module MapForTrees
+
+
+type Tree<'a> =
+    | Node of 'a * Tree<'a> * Tree<'a>
+    | Empty
+
+let rec map tree func =
+    match tree with 
+    | Empty -> Empty
+    | Node (value, left, right) -> Node(func value, map left func, map right func) 
+
