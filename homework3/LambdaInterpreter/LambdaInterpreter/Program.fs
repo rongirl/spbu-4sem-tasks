969d4c55ce639023b4336e6c348d4277abb0c26b
rongirl <gro.pretty.arabella@mail.ru>
2023-03-24 22:27:34 +0300

    Initial commit

---
+++ homework3/LambdaInterpreter/LambdaInterpreter/Program.fs
@@ -0,0 +1,17 @@
+﻿module LambdaInterpreter
+
+type Variable = string
+
+type Term =
+    | Variable of Variable
+    | Application of Term * Term
+    | LambdaAbstraction of Variable * Term
+
+let rec freeVariables term =
+    match term with
+    | Variable x -> Set.singleton(x)
+    | Application (S, T) -> Set.union (freeVariables S) (freeVariables T)
+    | LambdaAbstraction (variable, T) -> freeVariables T |> Set.remove variable
+
+let isFree variable term =
+    freeVariables term |> Set.contains variable
