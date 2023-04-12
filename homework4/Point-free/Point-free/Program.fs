74437e30ab31988c64d1348effa6657f895691e7
rongirl <gro.pretty.arabella@mail.ru>
2023-04-12 14:25:54 +0300

    Added task

---
+++ homework4/Point-free/Point-free/Program.fs
@@ -0,0 +1,13 @@
+﻿module PointFree
+
+let func x l = List.map (fun y -> y * x) l
+
+let func'1 x = List.map (fun y -> y * x)
+
+let func'2 x = List.map (fun y -> ((*) x) y)
+
+let func'3 x =  List.map ((*) x)
+
+let func'4 x = x |> (*) |> List.map
+
+let func'5 = (*) >> List.map
